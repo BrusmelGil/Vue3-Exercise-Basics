31dfa6e626c35f89689c4e243d433b37089ac035
Brusmel Gil <Brusmel@outlook.es>
2024-02-26 18:30:24 +0100

    finished

--- src/components/InputPassword.vue
+++ src/components/InputPassword.vue
@@ -1,14 +1,21 @@
 <script setup>
 import { ref } from 'vue';
 
+const password = ref("");
+const showPassword = ref(false);
+
+const  togglePasswordVisibility = () => {
+    showPassword.value = !showPassword.value;
+}
 </script>
 
 <template>
+
     <div class="container text-center input-password">
         <h1 class="my-3">Exercise Input Password</h1>
-        <input type="password" name="" id="password" class="form-control">
+        <input :type="showPassword ? 'text' : 'password'" v-model="password" name="password" id="password" class="form-control">
         <div class="d-block">
-            <span id="show-password" class="mt-2 btn btn-primary">Show Password</span>
+            <button id="show-password" class="mt-2 btn btn-primary" @click=" togglePasswordVisibility">Show Password</button>
         </div>
     </div>
 </template>
