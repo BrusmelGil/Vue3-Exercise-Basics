5b96dd2aab0e0897ab53e79d0790d0ff22210716
Brusmel Gil <Brusmel@outlook.es>
2024-02-26 18:38:36 +0100

    finished

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -2,6 +2,11 @@
 import { ref } from "vue";
 
 const title = ref("Exercise Read More");
+const showFullText = ref(false);
+
+const toggleFullTextVisibility = () => {
+    showFullText.value = !showFullText.value;
+}
 
 </script>
 
@@ -9,7 +14,7 @@ const title = ref("Exercise Read More");
   <div class="container text-center">
     <h1 class="my-3">{{ title }}</h1>
     <div>
-      <p class="mb-3 d-block cut-text">
+      <p :class="{'cut-text': !showFullText}" class="mb-3 d-block">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
         ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
         molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
@@ -17,7 +22,9 @@ const title = ref("Exercise Read More");
         temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
         quaerat autem non hic, quidem dolore laboriosam.
       </p>
-      <button class="btn btn-primary">Show More</button>
+      <button @click="toggleFullTextVisibility" class="btn btn-primary">
+        {{ showFullText ? 'Show Less' : 'Show More' }}
+      </button>
     </div>
     <hr class="mb-4" />
   </div>
